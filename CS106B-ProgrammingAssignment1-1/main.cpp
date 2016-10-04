b17b6fb1f47667c3a19a332356d89eab0241a39b
John Pierce <jjvppierce@sbcglobal.net>
2016-10-03 23:38:40 -0700

    Initial Commit

---
+++ CS106B-ProgrammingAssignment1-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS106B-ProgrammingAssignment1-1
+//
+//  Created by John Pierce on 10/3/16.
+//  Copyright © 2016 JohnPierce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
